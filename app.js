bce60fcae09be80e60a309e1415a0cf512894a06
unknown <yusuf.chnd@gmail.com>
2022-08-01 14:22:11 +0700

    light mode

--- app.js
+++ app.js
@@ -27,6 +27,13 @@ function PageTransitions(){
             element.classList.add('active');
         }
     })
+
+    //theme//
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click',() =>{
+        let element = document.body;
+        element.classList.toggle('light-mode');
+    })
 }
 
 PageTransitions();
